2f738220682fbf24fe80c60c2036286239084dd8
Arup ROY <99281775+karuproy@users.noreply.github.com>
2022-02-23 17:49:07 -0500

    Update Extracting_CovidData_for_Tableau.sql

--- Covid_Analysis_with_SQL_&_Tableau/Extracting_CovidData_for_Tableau.sql
+++ Covid_Analysis_with_SQL_&_Tableau/Extracting_CovidData_for_Tableau.sql
@@ -1,35 +1,41 @@
-/*
-Queries used for Tableau Project
-*/
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------- Statistics on Covid Infection until February, 2022 ---------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
--- 1. 
 
-Select SUM(new_cases) as total_cases_daily, SUM(cast(new_deaths as int)) as death_total_daily, SUM(cast(new_deaths as int))/SUM(new_cases)*100 as death_percent_daily
+-- 1. Total Cases, Deaths and Percentage
+
+Select SUM(new_cases) as cases_total_daily, SUM(cast(new_deaths as int)) as death_total_daily, SUM(cast(new_deaths as int))/SUM(new_cases)*100 as death_percent_daily
 From portfolio_project.dbo.covid_infect
 Where continent is not null 
-Order by 1,2
+Order by cases_total_daily, death_total_daily
 
 
--- 2. 
+
+-- 2. Daily Death counts in every Continent
 
 Select location, SUM(cast(new_deaths as int)) as death_total_daily
 From portfolio_project.dbo.covid_infect
 Where continent is null 
-and location not in ('World', 'European Union', 'International')
-and location not like '%income%'
+And location not in ('World', 'European Union', 'International')
+And location not like '%income%'
 Group by location
 Order by death_total_daily desc
 
 
--- 3.
+
+-- 3. Infection rates and Population of each Country
+
 Select location, population, MAX(total_cases) as infected_count_max,  Max((total_cases/population))*100 as infected_percent_max
 From portfolio_project.dbo.covid_infect
 Group by location, population
 Order by infected_percent_max desc
 
 
--- 4.
+
+-- 4. Time-Series of Daily Cases in each Country
 
 Select location, population, date, MAX(total_cases) as infected_count_max,  Max((total_cases/population))*100 as infected_percent_max
 From portfolio_project.dbo.covid_infect
@@ -37,117 +43,64 @@ Group by location, population, date
 Order by infected_percent_max desc
 
 
---------------------------------------------------------------------------------------------------------------------------------------------------
 
---------------------------------------------------------------------------------------------------------------------------------------------------
-
-
--- Queries I originally had, but excluded some because it created too long of video
--- Here only in case you want to check them out
-
-
--- 1.
-
-Select dea.continent, dea.location, dea.date, dea.population
-, MAX(vac.total_vaccinations) as RollingPeopleVaccinated
---, (RollingPeopleVaccinated/population)*100
-From PortfolioProject..CovidDeaths dea
-Join PortfolioProject..CovidVaccinations vac
-	On dea.location = vac.location
-	and dea.date = vac.date
-where dea.continent is not null 
-group by dea.continent, dea.location, dea.date, dea.population
-order by 1,2,3
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------- Statistics on Covid Vaccination until February, 2022 --------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
+-- 5. Time-series of Daily Vaccinations in each Country
 
--- 2.
-Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
-From PortfolioProject..CovidDeaths
---Where location like '%states%'
-where continent is not null 
---Group By date
-order by 1,2
-
-
--- Just a double check based off the data provided
--- numbers are extremely close so we will keep them - The Second includes "International"  Location
-
-
---Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
---From PortfolioProject..CovidDeaths
-----Where location like '%states%'
---where location = 'World'
-----Group By date
---order by 1,2
-
-
--- 3.
-
--- We take these out as they are not inluded in the above queries and want to stay consistent
--- European Union is part of Europe
-
-Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
-From PortfolioProject..CovidDeaths
---Where location like '%states%'
-Where continent is null 
-and location not in ('World', 'European Union', 'International')
-Group by location
-order by TotalDeathCount desc
+Select inf.continent, inf.location, inf.date, inf.population, 
+	MAX(vac.total_vaccinations) as RollingPeopleVaccinated --, (RollingPeopleVaccinated/population)*100
+From portfolio_project.dbo.covid_infect inf
+Join portfolio_project.dbo.covid_vaccine vac
+	On inf.location = vac.location
+	And inf.date = vac.date
+Where inf.continent is not null 
+Group by inf.continent, inf.location, inf.date, inf.population
+Order by inf.date desc--, RollingPeopleVaccinated desc
 
 
 
--- 4.
+-- 6. Total Population, Vaccination and Percentage 
 
-Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From PortfolioProject..CovidDeaths
---Where location like '%states%'
-Group by Location, Population
-order by PercentPopulationInfected desc
-
-
-
--- 5.
-
---Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
---From PortfolioProject..CovidDeaths
-----Where location like '%states%'
---where continent is not null 
---order by 1,2
-
--- took the above query and added population
-Select Location, date, population, total_cases, total_deaths
-From PortfolioProject..CovidDeaths
---Where location like '%states%'
-where continent is not null 
-order by 1,2
-
-
--- 6. 
-
-
-With PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
-as
-(
-Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
-, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
---, (RollingPeopleVaccinated/population)*100
-From PortfolioProject..CovidDeaths dea
-Join PortfolioProject..CovidVaccinations vac
-	On dea.location = vac.location
-	and dea.date = vac.date
-where dea.continent is not null 
---order by 2,3
-)
+With pop_vs_vac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated) 
+as	(	
+	Select	inf.continent, inf.location, inf.date, inf.population, 
+			vac.new_vaccinations, 
+			SUM(CONVERT(bigint,vac.new_vaccinations)) OVER (Partition by inf.Location Order by inf.location, inf.Date) as RollingPeopleVaccinated 
+			--, (RollingPeopleVaccinated/population)*100
+	From portfolio_project.dbo.covid_infect inf
+	Join portfolio_project.dbo.covid_vaccine vac
+		On inf.location = vac.location
+		And inf.date = vac.date
+	Where inf.continent is not null
+	)
 Select *, (RollingPeopleVaccinated/Population)*100 as PercentPeopleVaccinated
-From PopvsVac
+From pop_vs_vac
 
 
--- 7. 
 
-Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From PortfolioProject..CovidDeaths
---Where location like '%states%'
-Group by Location, Population, date
-order by PercentPopulationInfected desc
+-- 7. Canadian Stats on Covid
+
+Select inf.date, population, total_cases as accumulated_case, total_deaths as accumulated_death, 
+			SUM(CONVERT(bigint,vac.new_vaccinations)) OVER (Partition by inf.Location Order by inf.location, inf.Date) as accumulated_vaccination
+From portfolio_project.dbo.covid_infect inf
+Join portfolio_project.dbo.covid_vaccine vac
+	On inf.location = vac.location
+	And inf.date = vac.date
+Where inf.location like '%canada%'
+Order by inf.date
+
+
+
+-- 8. Accumulated Cases and Deaths of all Countries after every Month (Data Extarction for PowerBI)
+
+Select location as Country, cast(date as varchar), total_cases as Accumulated_Case, total_deaths as Accumulated_Death, population as Populations
+From portfolio_project.dbo.covid_infect
+Where cast(date as varchar) like '%Feb  1 2022%'
+-- Where cast(date as varchar) like '%  1 20%'
+and continent is not null 
+Order by location, date desc
